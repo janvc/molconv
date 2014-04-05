b4b445ebf0f0fd835a1d5a383d6e5f61ab87ef0a
Sebastian Lenz <sebastian@slenz.net>
2014-04-05 14:24:20 +0200

    added exception handling for whole main function

--- source/molconv.cpp
+++ source/molconv.cpp
@@ -41,78 +41,89 @@ namespace
 int main(int argc, char *argv[])
 {
 
-	std::string inputfile = "";
-	std::string appName = boost::filesystem::basename(argv[0]);
-
-	// Declare the supported options.
-	po::options_description opts;
-	opts.add_options()
-		("help,h", "Display help message")
-	;
-
-    po::options_description hidden("Hidden options");
-    hidden.add_options()
-		("inputfiles",po::value< std::vector<std::string> >()->required(), "inputfiles")
-    ;
-
-	po::positional_options_description positionalOptions;
-	positionalOptions.add("inputfiles", -1);
-
-    po::options_description cmdline_opts;
-    cmdline_opts.add(opts).add(hidden);
-
-    po::options_description visible("Options");
-    visible.add(opts);
-
-	po::variables_map vm;
-
-	try
+    try
 	{
-		po::store(po::command_line_parser(argc, argv).options(cmdline_opts)
-					.positional(positionalOptions).run(),
-				  vm); // throws on error
+		std::string inputfile = "";
+		std::string appName = boost::filesystem::basename(argv[0]);
 
-		if( vm.count("help") )
+		// Declare the supported options.
+		po::options_description opts;
+		opts.add_options()
+			("help,h", "Display help message")
+		;
+
+		po::options_description hidden("Hidden options");
+		hidden.add_options()
+			("inputfiles",po::value< std::vector<std::string> >()->required(), "inputfiles")
+		;
+
+		po::positional_options_description positionalOptions;
+		positionalOptions.add("inputfiles", -1);
+
+		po::options_description cmdline_opts;
+		cmdline_opts.add(opts).add(hidden);
+
+		po::options_description visible("Options");
+		visible.add(opts);
+
+		po::variables_map vm;
+
+		try
 		{
-            print_help_msg(appName);
-			return SUCCESS;
+			po::store(po::command_line_parser(argc, argv).options(cmdline_opts)
+						.positional(positionalOptions).run(),
+					  vm); // throws on error
+
+			if( vm.count("help") )
+			{
+				print_help_msg(appName);
+				return SUCCESS;
+			}
+
+			po::notify(vm);
+		}
+		catch(boost::program_options::required_option& e)
+		{
+			std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+			return ERROR_IN_COMMAND_LINE;
+		}
+		catch(boost::program_options::error& e)
+		{
+			std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+			return ERROR_IN_COMMAND_LINE;
 		}
 
-		po::notify(vm);
+
+
+
+		//    configuration input_paras(argc, argv);
+
+		//    if (input_paras.help_wanted())
+		//    {
+		//    	print_help_msg();
+		//    	return 0;
+		//    }
+		//    else
+		//    	print_header();
+
+
+		//    if (input_paras.input_exists())
+		//    {
+		//        molecule testmolecule(input_paras.get_inputfile().c_str());
+
+		//        if (input_paras.output_exists())
+		//        	testmolecule.write_to_file(input_paras.get_outputfile().c_str());
+		//    }
+
 	}
-	catch(boost::program_options::required_option& e)
+	catch(std::exception& e)
 	{
-		std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
-		return ERROR_IN_COMMAND_LINE;
-	}
-	catch(boost::program_options::error& e)
-	{
-		std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
-		return ERROR_IN_COMMAND_LINE;
+
+		std::cerr << "Unhandled Exception reached the top of main: "
+				  << e.what() << ", application will now exit" << std::endl;
+		return ERROR_UNHANDLED_EXCEPTION;
 	}
 
-
-
-
-	//    configuration input_paras(argc, argv);
-
-	//    if (input_paras.help_wanted())
-	//    {
-	//    	print_help_msg();
-	//    	return 0;
-	//    }
-	//    else
-	//    	print_header();
-
-
-	//    if (input_paras.input_exists())
-	//    {
-	//        molecule testmolecule(input_paras.get_inputfile().c_str());
-
-	//        if (input_paras.output_exists())
-	//        	testmolecule.write_to_file(input_paras.get_outputfile().c_str());
-	//    }
-
-	return 0;
+	return SUCCESS;
 }
 
