6b693a22a8fdd34d10fe05859e03a22e6ea672b0
jan <jan@kritzkrieg>
2014-04-20 15:54:13 +0200

    added explanatory comments to most of the functions

--- source/atom.cpp
+++ source/atom.cpp
@@ -25,6 +25,9 @@
 
 namespace molconv
 {
+	/*
+	 * The constructor of the atom class.
+	 */
 	atom::atom(int at_num, Eigen::Vector3d pos)
 	{
 		this->atomicnumber = at_num;
@@ -36,54 +39,82 @@ namespace molconv
 	}
 
 
+	/*
+	 * This function shifts the atom about a specified shift vector.
+	 */
 	void atom::shift(Eigen::Vector3d shift_vector)
 	{
 		this->position += shift_vector;
 	}
 
 
+	/*
+	 * This function returns the atomic number of an atom.
+	 */
 	int atom::get_atomicnumber()
 	{
 		return this->atomicnumber;
 	}
 
 
+	/*
+	 * This function returns the atomic symbol of an atom.
+	 */
 	std::string atom::get_atomicsymbol()
 	{
 		return this->atomicsymbol;
 	}
 
 
+	/*
+	 * This function returns the mass of an atom.
+	 */
 	double atom::get_atomicmass()
 	{
 		return this->mass;
 	}
 
 
+	/*
+	 * This function returns the internal position vector of an atom.
+	 */
 	Eigen::Vector3d atom::get_int_position()
 	{
 		return this->position;
 	}
 
 
+	/*
+	 * This function returns the x-value of an atoms internal position.
+	 */
 	double atom::get_int_x()
 	{
 		return this->position(0);
 	}
 
 
+	/*
+	 * This function returns the y-value of an atoms internal position.
+	 */
 	double atom::get_int_y()
 	{
 		return this->position(1);
 	}
 
 
+	/*
+	 * This function returns the z-value of an atoms internal position.
+	 */
 	double atom::get_int_z()
 	{
 		return this->position(2);
 	}
 
 
+	/*
+	 * This function transforms the position vector of an atom by
+	 * multiplying it with a matrix.
+	 */
 	void atom::transform(Eigen::Matrix3d tmatrix)
 	{
 		this->position = tmatrix * this->position;
--- source/molecule.cpp
+++ source/molecule.cpp
@@ -30,6 +30,11 @@
 
 namespace molconv
 {
+	/*
+	 * The constructor of the molecule class. This constructor reads a (xyz-) molecular structure file
+	 * and sets up the internal coordinate representation of the molecular structure. It calculates and
+	 * diagonalizes the inertia tensor and performs a prinicpal axis transformation.
+	 */
 	molecule::molecule(const char *input_file)
 	{
 		std::ifstream input(input_file);
@@ -135,12 +140,19 @@ namespace molconv
 	}
 
 
+	/*
+	 * This function translates the entire molecule about a specified shift vector
+	 */
 	void molecule::shift(Eigen::Vector3d shift_vector)
 	{
 		this->internal_origin += shift_vector;
 	}
 
 
+	/*
+	 * This function writes the molecular structure to the standard output stream, in the same format
+	 * as it would appear in an xyz-File. The coordinates are printed in the basis of the external space.
+	 */
 	void molecule::print_stdout()
 	{
 		std::cout << "The number of atoms is: " << this->number_of_atoms << std::endl;
@@ -159,12 +171,18 @@ namespace molconv
 	}
 
 
+	/*
+	 * this function returns the comment-line from the xyz-file that was read.
+	 */
 	std::string molecule::get_commentline()
 	{
 		return this->comment_line;
 	}
 
 
+	/*
+	 * This function prints some information about the molecule to the standard output stream.
+	 */
 	void molecule::show_info()
 	{
 		std::cout << "Molecular mass: " << std::endl << this->mass << " u" << std::endl;
@@ -180,6 +198,9 @@ namespace molconv
 	}
 
 
+	/*
+	 * This function calculates the total mass of the molecule.
+	 */
 	void molecule::calc_mass()
 	{
 		this->mass = 0.0;
@@ -191,6 +212,9 @@ namespace molconv
 	}
 
 
+	/*
+	 * This function calculates the position of the (mass-weighted) center-of-mass of the molecule.
+	 */
 	void molecule::calc_com()
 	{
 		this->center_of_mass << 0.0, 0.0, 0.0;
@@ -204,6 +228,10 @@ namespace molconv
 	}
 
 
+	/*
+	 * This function calculates the position of the (non-mass-weighted) center-of-geometry of the
+	 * molecule, i.e. the average position of the atoms.
+	 */
 	void molecule::calc_cog()
 		{
 			this->center_of_geometry << 0.0, 0.0, 0.0;
@@ -217,6 +245,18 @@ namespace molconv
 		}
 
 
+	/*
+	 * This function calculates the inertia tensor of the molecule. The elements of the inertia tensor
+	 * are given by:
+	 * 					J_ij = sum_k=1^N  m_k * ((r_k)^2 * delta_ij) - x_i * x_j
+	 *
+	 * This results in:
+	 * 						 / y^2 + z^2    - x * y     - x * z  \
+	 * 					J = |   - x * y    x^2 + z^2    - y * z   |
+	 * 					     \  - x * z     - y * z    x^2 + y^2 /
+	 *
+	 * where the sum is implicitly included in the matrix elements.
+	 */
 	void molecule::calc_inertia()
 	{
 		for (int alpha = 0; alpha < 3; alpha++)
@@ -239,6 +279,12 @@ namespace molconv
 	}
 
 
+	/*
+	 * This function diagonalizes the inertia tensor using a routine from the EIGEN library.
+	 * The eigenvectors of the inertia tensor are the principal axes of the molecule and these are
+	 * used as the coordinate axes for the molecule fixed coordinate system. The eigenvalues of
+	 * the inertia tensor are the moments of inertia along those axes.
+	 */
 	void molecule::diag_inertia()
 	{
 		Eigen::SelfAdjointEigenSolver<Eigen::Matrix3d> solver(this->inertia_tensor);
@@ -255,6 +301,10 @@ namespace molconv
 	}
 
 
+	/*
+	 * This function writes the molecular structure (in absolute coordinates) to a text file
+	 * using the xyz format.
+	 */
 	bool molecule::write_to_file(const char *outputfile)
 	{
 		std::ofstream output(outputfile);
@@ -279,6 +329,11 @@ namespace molconv
 	}
 
 
+	/*
+	 * This function shifts the origin of the internal coordinates into the origin of the external
+	 * space and sets the internal basis to the unit matrix. After cleaning up, the absolute coordinates
+	 * are equal to the internal coordinates.
+	 */
 	void molecule::clean_up()
 	{
 		this->internal_origin << 0,0,0;
@@ -288,6 +343,20 @@ namespace molconv
 	}
 
 
+	/*
+	 * This function calculates the covariance matrix of the molecule. The variance is a measure of the
+	 * spread of the atoms in the respective direction. The covariance matrix is calculated as:
+	 *
+	 * 				C_ij = 1/N * sum_k=1^N  (x_i - u_i) * (x_j - u_j)
+	 *
+	 * or in matrix form:
+	 * 							/     (x - u_x)^2      (x - u_x)*(y - u_y)  (x - u_x)*(y - u_y) \
+	 * 				C = 1/N *  |  (x - u_x)*(y - u_y)      (y - u_y)^2      (y - u_y)*(z - u_z)  |
+	 * 							\ (x - u_x)*(y - u_y)  (y - u_y)*(z - u_z)      (z - u_z)^2     /
+	 *
+	 * where u_x, u_y, u_z are the positions of the GEOMETRIC center of the molecule (i.e. the center
+	 * of geometry, see respective function above).
+	 */
 	void molecule::calc_covar_mat()
 	{
 		for (int i = 0; i < 3; i++)
@@ -307,6 +376,12 @@ namespace molconv
 	}
 
 
+	/*
+	 * This function diagonalizes the covariance matrix. The eigenvectors of the covariance matrix are
+	 * the directions with the minimum and maximum variance, respectively, while the corresponding
+	 * eigenvalues are the variances along these directions. For example, the eigenvector corresponding
+	 * to the lowest variance stands perpendicular to the least squares plane of the molecule.
+	 */
 	void molecule::diag_covar_mat()
 	{
 		Eigen::SelfAdjointEigenSolver<Eigen::Matrix3d> solver(this->covar_mat);
@@ -324,6 +399,11 @@ namespace molconv
 	}
 
 
+	/*
+	 * This function calculates the elements of the transformation matrix from the eulerian angles
+	 * describing the rotations from the external coordinate system to the internal coordinate system.
+	 * the definition of the eulerian angles is adapted from: H. Goldstein, "classical mechanics".
+	 */
 	void molecule::euler2trans()
 	{
 		this->internal_basis(0,0) =  cos(this->euler_psi) * cos(this->euler_phi)
