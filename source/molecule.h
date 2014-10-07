45f9a2fa3d17ca2271797fe21bf20b31d017d11b
Jan von Cosel <jvc_astronx@gmx.de>
2014-10-07 17:15:46 +0200

    started reworking the molecule class (not working yet)

--- source/molecule.h
+++ source/molecule.h
@@ -29,50 +29,25 @@
 #include<Eigen/Core>
 #include "types.h"
 
-namespace molconv {
-    class Molecule : public chemkit::Molecule {
-     public:
-         Molecule();                                                          // the default constructor
-         Molecule(const chemkit::Molecule &base_molecule);                    // take a base molecule
-         Molecule(const boost::shared_ptr<chemkit::Molecule> &base_mol_ptr);  // same as above, but with pointer
+namespace molconv
+{
+    class MoleculePrivate;
 
-         void show_inertia();                                                 // show the inertia tensor
-         void show_covariance();                                              // show the covariance matrix
-         void rotate(Eigen::Matrix3d rotation_matrix);                        // rotate the molecule about the origin with a rotation matrix
-         void clean_up();                                                     // clean up the coordinates of the molecule
-         void set_basis(const origin origin, const basis axes, const int origin_atom=0, const int basis_atom1=0, const int basis_atom2=0, const int basis_atom3=0);
-     private:
-         void trans2euler();       // calculate the eulerian angles from the transformation matrix
-         void euler2trans();       // calculate the transformation matrix from the eulerian angles
+    class Molecule : public chemkit::Molecule
+    {
+    public:
+        Molecule();
+        Molecule(const chemkit::Molecule &BaseMolecule);
+        Molecule(const Molecule &originalMolecule);
 
-         void calc_inertia_tensor();           // calculate the tensor of moments of inertia
-         void diag_inertia_tensor();           // diagonalize the inertia tensor
-         void calc_covariance_matrix();        // calculate the covariance matrix of the molecule
-         void diag_covariance_matrix();        // diagonalize the covariance matrix
-         void update_geometrical_props();      // update the geometrical properties (i.e. after an atomic coordinate change)
+        void shift(const Eigen::Vector3d &shiftVector);
+        void rotate(const Eigen::Matrix3d &rotationMatrix);
 
-         size_t number_of_atoms_;              // the number of atoms in the molecule
-         double total_mass_;                   // total mass of the molecule
-         Eigen::Vector3d center_of_mass_;      // the center of mass of the molecule
-         Eigen::Vector3d center_of_geometry_;  // the center of geometry of the molecule
-         Eigen::Matrix3d inertia_tensor_;      // tensor of the moments of inertia
-         Eigen::Vector3d inertia_eigvals_;     // the eigenvalues of the inertia tensor
-         Eigen::Matrix3d inertia_eigvecs_;     // the eigenvectors of the inertia tensor (the principal axes)
-         Eigen::Matrix3d covariance_matrix_;        // the covariance matrix of the molecule
-         Eigen::Vector3d covariance_eigvals_;       // eigenvalues of the covariance matrix
-         Eigen::Matrix3d covariance_eigvecs_;       // eigenvectors of the covariance matrix
-
-         double euler_phi_;                    // |
-         double euler_theta_;                  // | the eulerian angles specifying the orientation of the molecule
-         double euler_psi_;                    // |
-         Eigen::Vector3d origin_;              // origin of the internal coordinate system
-         Eigen::Matrix3d basis_;               // the basis vectors of the internal coordinate system in terms
-         size_t origin_type_;                  // internal origin
-         size_t origin_atom_;                  // atom that defines the internal origin (if set)
-         size_t basis_type_;                   // internal basis
-         std::vector<size_t> basis_atoms_;     // atoms that define the internal basis (if set)
-                                               // of the global coordinates
+    private:
+        MoleculePrivate *d;
     };
+
+
 } // namespace molconv
 
 #endif /* MOLECULE_H_ */
