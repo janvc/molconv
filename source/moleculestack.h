6b5b42e3ddd6571ad18983e8fcedfdf0a7892372
Jan von Cosel <jvc_astronx@gmx.de>
2014-10-22 10:58:58 +0200

    added some functions to class MoleculeStack

--- source/moleculestack.h
+++ source/moleculestack.h
@@ -12,6 +12,16 @@ namespace molconv
     public:
         MoleculeStack();
 
+        double Distance(const size_t index) const;
+        double RotationAngle(const size_t index) const;
+        double LateralX(const size_t index) const;
+        double LateralY(const size_t index) const;
+
+        void setDistance(const size_t index, const double newDistance);
+        void setRotationAngle(const size_t index, const double newAngle);
+        void setLateralX(const size_t index, const double newX);
+        void setLateralY(const size_t index, const double newY);
+
     private:
         MoleculeStackPrivate *d;
     };
