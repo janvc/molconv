42c7c07db0e48c3ec236556b306773f8351882ad
Jan von Cosel <jvc_astronx@gmx.de>
2014-10-25 19:09:02 +0200

    implemented some functions in class MoleculeStack

--- source/moleculestack.h
+++ source/moleculestack.h
@@ -1,3 +1,24 @@
+/*
+ * Copyright 2014 Jan von Cosel & Sebastian Lenz
+ *
+ * This file is part of molconv.
+ *
+ * molconv is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * molconv is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have recieved a copy of the GNU General Public License
+ * along with molconv. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
 #ifndef MOLECULESTACK_H
 #define MOLECULESTACK_H
 
@@ -12,12 +33,15 @@ namespace molconv
     public:
         MoleculeStack();
 
-        double Distance(const size_t index) const;
+        size_t ReferenceMolecule() const;
+        void setReferenceMolecule(const size_t newRef);
+
+        double PlaneDistance(const size_t index) const;
         double RotationAngle(const size_t index) const;
         double LateralX(const size_t index) const;
         double LateralY(const size_t index) const;
 
-        void setDistance(const size_t index, const double newDistance);
+        void setPlaneDistance(const size_t index, const double newDistance);
         void setRotationAngle(const size_t index, const double newAngle);
         void setLateralX(const size_t index, const double newX);
         void setLateralY(const size_t index, const double newY);
