e105807b4c17166b23b9c4dcf1cfb4804a84ba34
Jan von Cosel <janvc@gmx.de>
2017-01-02 17:24:06 +0100

    implemented mouse move and release events in selecttool

--- source/selecttool.cpp
+++ source/selecttool.cpp
@@ -20,6 +20,7 @@
 
 #include <chemkit/graphicsitem.h>
 #include <chemkit/graphicsview.h>
+#include <chemkit/graphicsoverlay.h>
 #include "selecttool.h"
 #include "molconv_window.h"
 
@@ -32,6 +33,16 @@ public:
     }
 
     MolconvWindow *m_window;
+    QGraphicsRectItem *m_rectItem;
+    QPointF m_initPos;
+    QPointF m_lastPos;
+
+    enum State
+    {
+        Selecting,
+        Deselecting
+    }
+    m_state;
 };
 
 SelectTool::SelectTool(MolconvWindow *newWindow)
@@ -47,6 +58,14 @@ SelectTool::~SelectTool()
 
 void SelectTool::mousePressEvent(QMouseEvent *event)
 {
+    d->m_initPos = event->localPos();
+    d->m_lastPos = event->localPos();
+
+    if (event->buttons() & Qt::LeftButton)
+        d->m_state = SelectToolPrivate::Selecting;
+    else if (event->buttons() & Qt::RightButton)
+        d->m_state = SelectToolPrivate::Deselecting;
+
     chemkit::GraphicsItem *item = view()->itemAt(event->x(), event->y());
 
     if (item && item->type() == chemkit::GraphicsItem::AtomItem)
@@ -54,29 +73,61 @@ void SelectTool::mousePressEvent(QMouseEvent *event)
         chemkit::GraphicsAtomItem *atomItem = static_cast<chemkit::GraphicsAtomItem *>(item);
         chemkit::Atom *atom = const_cast<chemkit::Atom *>(atomItem->atom());
 
-        if (event->buttons() & Qt::LeftButton)
+        if (d->m_state == SelectToolPrivate::Selecting)
             if (event->modifiers() & Qt::ShiftModifier)
                 d->m_window->selectAtom(atom, true);
             else
                 d->m_window->selectAtom(atom, false);
 
-        if (event->buttons() & Qt::RightButton)
+        if (d->m_state == SelectToolPrivate::Deselecting)
             if (event->modifiers() & Qt::ShiftModifier)
                 d->m_window->deselectAtom(atom, true);
             else
                 d->m_window->deselectAtom(atom, false);
-
     }
+
+    d->m_rectItem = new QGraphicsRectItem(event->x(), event->y(), 0, 0);
+    d->m_rectItem->setPen(QPen(QBrush(Qt::white), 2, Qt::DashLine));
+    d->m_rectItem->setOpacity(0.5);
+    view()->overlay()->addItem(d->m_rectItem);
 }
 
 void SelectTool::mouseMoveEvent(QMouseEvent *event)
 {
-    Q_UNUSED(event);
+    d->m_lastPos = event->localPos();
+    d->m_rectItem->setRect(QRectF(d->m_initPos, event->pos()).normalized());
+    view()->update();
 }
 
 void SelectTool::mouseReleaseEvent(QMouseEvent *event)
 {
-    Q_UNUSED(event);
+    QRect rect = d->m_rectItem->rect().toRect().normalized();
+    for (int x = rect.x(); x < rect.right(); x+= 5)
+        for (int y = rect.y(); y < rect.bottom(); y+= 5)
+        {
+            chemkit::GraphicsItem *item = view()->itemAt(x, y);
+            if (item && item->type() == chemkit::GraphicsItem::AtomItem)
+            {
+                chemkit::GraphicsAtomItem *atomItem = static_cast<chemkit::GraphicsAtomItem *>(item);
+                chemkit::Atom *atom = const_cast<chemkit::Atom *>(atomItem->atom());
+                if (d->m_state == SelectToolPrivate::Selecting)
+                    if (event->modifiers() & Qt::ShiftModifier)
+                        d->m_window->selectAtom(atom, true);
+                    else
+                        d->m_window->selectAtom(atom, false);
+
+                if (d->m_state == SelectToolPrivate::Deselecting)
+                    if (event->modifiers() & Qt::ShiftModifier)
+                        d->m_window->deselectAtom(atom, true);
+                    else
+                        d->m_window->deselectAtom(atom, false);
+            }
+        }
+
+    view()->overlay()->removeItem(d->m_rectItem);
+    delete d->m_rectItem;
+    d->m_rectItem = 0;
+    view()->update();
 }
 
 void SelectTool::wheelEvent(QWheelEvent *event)
