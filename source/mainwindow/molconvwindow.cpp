2e2f4811926c19937a4109dbdb27929d15a6ab0a
Jan von Cosel <janvc@gmx.de>
2020-05-15 21:34:50 +0200

    fixed a bug where axes would not move properly

--- source/mainwindow/molconvwindow.cpp
+++ source/mainwindow/molconvwindow.cpp
@@ -765,19 +765,25 @@ void MolconvWindow::wasModified()
     ui->actionSave->setEnabled(true);
 }
 
+void MolconvWindow::moveActiveMoleculeTo(const double x, const double y, const double z,
+                                         const double phi, const double theta, const double psi)
+{
+    getMol(d->m_activeMolID)->moveFromParas(x, y, z, phi, theta, psi);
+    d->m_MoleculeSettings->setMolecule(d->m_activeMolID);
+    updateAxes();
+    d->m_MoleculeInfo->updateLive();
+    wasModified();
+}
+
 void MolconvWindow::resetCoords()
 {
     std::array<double,6> oB = d->m_system->getMolecule(d->m_activeMolID)->origBasis();
-    d->m_system->getMolecule(d->m_activeMolID)->moveFromParas(oB[0], oB[1], oB[2], oB[3], oB[4], oB[5]);
-    d->m_MoleculeSettings->setMolecule(d->m_activeMolID);
-//    d->m_MoleculeSettings->moveMolecule(oB[0], oB[1], oB[2], oB[3], oB[4], oB[5]);
+    moveActiveMoleculeTo(oB[0], oB[1], oB[2], oB[3], oB[4], oB[5]);
 }
 
 void MolconvWindow::zeroCoords()
 {
-    d->m_system->getMolecule(d->m_activeMolID)->moveFromParas(0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
-    d->m_MoleculeSettings->setMolecule(d->m_activeMolID);
-//    d->m_MoleculeSettings->moveMolecule(0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+    moveActiveMoleculeTo(0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
 }
 
 void MolconvWindow::alignMolecules(std::vector<unsigned long> &molecules)
